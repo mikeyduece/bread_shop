831c5ae810821ea4a60bc73a0f8bb288bcc0ddb7
Mike Heft <mikeheft@gmail.com>
2018-02-20 06:18:53 -0700

    Add test and method to calculate the bakers percentage. The bakers percentage is a percentage of the weight of the flour.

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -3,10 +3,14 @@ class RecipeIngredient < ApplicationRecord
   belongs_to :ingredient
   validates :amount, presence: true
 
-  after_validation :get_bp
+  def bp
+    get_bp
+  end
+
+  private
 
   def get_bp
     flour_amt = recipe.flour_amts
-    self.bp = (amount / flour_amt) * 100
+    (amount / flour_amt) * 100
   end
 end
--- spec/models/recipe_ingredient_spec.rb
+++ spec/models/recipe_ingredient_spec.rb
@@ -18,7 +18,14 @@ RSpec.describe RecipeIngredient, type: :model do
       rec_ing_7 = RecipeIngredient.create(recipe_id: recipe.id, ingredient_id: ing_list[4].id, amount: 30)
       rec_ing_8 = RecipeIngredient.create(recipe_id: recipe.id, ingredient_id: ing_list[5].id, amount: 20)
 
-      expect(rec_ing_1.bp).to eq(30)
+      expect(rec_ing_1.bp).to eq(25.0)
+      expect(rec_ing_2.bp).to eq(75.0)
+      expect(rec_ing_3.bp).to eq(12.5)
+      expect(rec_ing_4.bp).to eq(5.0)
+      expect(rec_ing_5.bp).to eq(2.5)
+      expect(rec_ing_6.bp).to eq(6.25)
+      expect(rec_ing_7.bp).to eq(7.5)
+      expect(rec_ing_8.bp).to eq(5.0)
     end
   end
 end
