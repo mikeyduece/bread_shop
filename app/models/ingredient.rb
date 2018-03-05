f865d1ede33ee4ea207e8f74383dded74c96dac8
Mike Heft <mikeheft@gmail.com>
2018-03-05 10:22:48 -0700

    Add callback method to assign category to Ingredient model

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -3,7 +3,43 @@ class Ingredient < ApplicationRecord
   has_many :recipes, through: :recipe_ingredients
   validates :name, uniqueness: true
 
+  before_save :assign_category
+
   def self.create_list(list)
     list.map { |name| Ingredient.find_or_create_by(name: name) }
+  end
+
+  def assign_category
+    if sweeteners.include?(self[:name])
+      self[:category] = 'sweetener'
+    elsif fats.include?(self[:name])
+      self[:category] = "fat"
+    elsif water.include?(self[:name])
+      self[:category] = 'water'
+    elsif flours.include?(self[:name])
+      self[:category] = 'flour'
+    end
+  end
+
+  private
+
+  def sweeteners
+    ['sugar', 'brown sugar', 'corn syrup', 'agave', 'molasses',
+     'honey', 'maple syrup']
+  end
+
+  def fats
+    ['butter', 'milk', 'cream', 'sour cream', 'canola oil',
+     'olive oil', 'margerine']
+  end
+
+  def water
+    ['water']
+  end
+
+  def flours
+    ['flour', 'bread flour', 'high gluten flour', 'ap flour',
+     'all purpose flour', 'spelt', 'wheat flour', 'whole wheat flour',
+     'cake flour', 'pastry flour', 'semolina', 'durum', 'corn meal', 'flax meal']
   end
 end
--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -2,4 +2,22 @@ require 'rails_helper'
 
 RSpec.describe Ingredient, type: :model do
   it {should validate_uniqueness_of :name}
+
+  it 'can assign categories' do
+    fat_1 = Ingredient.create(name: 'butter')
+    fat_2 = Ingredient.create(name: 'cream')
+    sweet_1 = Ingredient.create(name: 'brown sugar')
+    sweet_2 = Ingredient.create(name: 'honey')
+    flour_1 = Ingredient.create(name: 'bread flour')
+    flour_2 = Ingredient.create(name: 'semolina')
+    water = Ingredient.create(name: 'water')
+
+    expect(fat_1.category).to eq('fat')
+    expect(fat_2.category).to eq('fat')
+    expect(sweet_1.category).to eq('sweetener')
+    expect(sweet_2.category).to eq('sweetener')
+    expect(flour_1.category).to eq('flour')
+    expect(flour_2.category).to eq('flour')
+    expect(water.category).to eq('water')
+  end
 end
