ac61a8b324e6a60499fbd1bfee23c08c92796e95
Mike Heft <mikeheft@gmail.com>
2018-04-16 09:46:22 -0600

    Add users#index to display all users

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       namespace :users do
+        get 'all', to: 'users#index'
         get ':email/recipes', to: 'recipes#index', constraints: { email: /.+@.+\..*/ }
         get ':email/recipes/:recipe_name', to: 'recipes#show', constraints: { email: /.+@.+\..*/ }
         post ':email/recipes', to: 'recipes#create', constraints: { email: /.+@.+\..*/ }
--- spec/requests/api/v1/users/users_request_spec.rb
+++ spec/requests/api/v1/users/users_request_spec.rb
@@ -2,9 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'User Requests' do
   let!(:users) { create_list(:user, 10) }
-  xit '#GET all users' do
+  # TODO Make this to where only an admin can see this list?
+  it '#GET all users' do
     get '/api/v1/users/all'
 
     expect(response).to be_success
+
+    user_list = JSON.parse(response.body, symbolize_names: true)
+
+    expect(user_list.count).to eq(10)
   end
 end
