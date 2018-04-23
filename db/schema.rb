8f6bbffd1f5aafa2bad64fa3b5f9aa11654129dd
Mike Heft <mikeheft@gmail.com>
2018-04-23 09:39:11 -0600

    Fix migration errors

--- db/schema.rb
+++ db/schema.rb
@@ -10,31 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180417004504) do
+ActiveRecord::Schema.define(version: 20180423153424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "comments", force: :cascade do |t|
-    t.bigint "user_id"
-    t.text "body"
-    t.string "title"
-    t.bigint "recipe_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["recipe_id"], name: "index_comments_on_recipe_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
-  end
-
-  create_table "follows", force: :cascade do |t|
-    t.integer "target_id"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["target_id", "user_id"], name: "index_follows_on_target_id_and_user_id", unique: true
-    t.index ["target_id"], name: "index_follows_on_target_id"
-    t.index ["user_id"], name: "index_follows_on_user_id"
-  end
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
@@ -78,15 +57,6 @@ ActiveRecord::Schema.define(version: 20180417004504) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "user_recipes", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "recipe_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["recipe_id"], name: "index_user_recipes_on_recipe_id"
-    t.index ["user_id"], name: "index_user_recipes_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "uid"
@@ -98,8 +68,7 @@ ActiveRecord::Schema.define(version: 20180417004504) do
 
   add_foreign_key "recipe_ingredients", "ingredients"
   add_foreign_key "recipe_ingredients", "recipes"
+  add_foreign_key "recipe_tags", "recipes"
   add_foreign_key "recipe_tags", "tags"
   add_foreign_key "recipes", "users"
-  add_foreign_key "user_recipes", "recipes"
-  add_foreign_key "user_recipes", "users"
 end
