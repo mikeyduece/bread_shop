11b4397e85a6e5be7439d67f5c0483510e1c3157
Mike Heft <mikeheft@gmail.com>
2018-05-05 16:25:24 -0600

    Add test for recipe name presence validation

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Recipe, 'validations' do
   it { should validate_uniqueness_of :name }
+  it { should validate_presence_of :name }
 end
 
 RSpec.describe Recipe, 'associations' do
