9f24f1ee71e62361796fde02e5aee022c7c8087f
Mike Heft <mikeheft@gmail.com>
2018-06-18 15:43:06 -0600

    Add nutrition label service

---
+++ app/services/nutrition_label_service.rb
@@ -0,0 +1,31 @@
+#frozen_string_literal: true
+
+class NutritionLabelService
+  def initialize(recipe)
+    @recipe = recipe
+    @conn = Faraday.new(url: "https://api.edamam.com/api/nutrition-details") do |faraday|
+      faraday.params["app_id"] = "#{ENV['label_app_id']}"
+      faraday.params["app_key"] = "#{ENV['label_app_key']}"
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+
+  def post_url
+    response = @conn.post do |req|
+      req.headers['Content-Type'] = 'application/json'
+      req.body = "{
+        'title': #{recipe[:title]},
+        'ingr': #{recipe[:ingr]}
+      }"
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.analyze_recipe(recipe)
+    new(recipe).post_url
+  end
+
+
+  private
+    attr_reader :recipe
+end
---
+++ spec/services/nutrition_label_service_spec.rb
@@ -0,0 +1,31 @@
+#frozen_string_litereal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Nutrition Label Service' do
+  let!(:recipe) {{
+    title: 'Baguette',
+    ingr: [
+      '1lb of flour',
+      '10 oz of water',
+      '1/4 oz yeast',
+      '1/3 oz salt'
+    ]
+  }}
+  it 'exists' do
+    VCR.use_cassette('label_service') do
+      label = NutritionLabelService.new(recipe)
+
+      expect(label).to be_a(NutritionLabelService)
+    end
+  end
+
+  it 'returns an analyzed recipe' do
+    VCR.use_cassette('analyzation') do
+      label = NutritionLabelService.analyze_recipe(recipe)
+
+      require 'pry'; binding.pry
+      expect(label).to be_a(NutritionLabel)
+    end
+  end
+end
