268561b53b241533f02b17b4f1d86eb86def9509
Mike Heft <mikeheft@gmail.com>
2018-06-19 12:22:15 -0600

    Add column to recipe for nutrition label, hstore

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180510125920) do
+ActiveRecord::Schema.define(version: 2018_06_19_182037) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "hstore"
   enable_extension "plpgsql"
 
   create_table "follows", force: :cascade do |t|
@@ -68,6 +69,7 @@ ActiveRecord::Schema.define(version: 20180510125920) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "family"
+    t.hstore "label"
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
