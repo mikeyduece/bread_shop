42ced4d305007acd15f09a4000fec3cf087c7b16
Mike Heft <mikeheft@gmail.com>
2018-06-19 10:11:25 -0600

    Add test to show return from service includes predetermined keys

--- spec/services/nutrition_label_service_spec.rb
+++ spec/services/nutrition_label_service_spec.rb
@@ -3,19 +3,19 @@
 require 'rails_helper'
 
 RSpec.describe 'Nutrition Label Service' do
-  let!(:recipe) {{
+  let!(:baguette) {{
     title: 'Baguette',
     ingr: [
       '1lb of flour',
-      '10 oz of water',
-      '1/4 oz yeast',
-      '1/3 oz salt'
+      '10oz of water',
+      '1/4oz yeast',
+      '1/3oz salt'
     ]
   }}
 
   it 'exists' do
     VCR.use_cassette('label_service') do
-      label = NutritionLabelService.new(recipe)
+      label = NutritionLabelService.new(baguette)
 
       expect(label).to be_a(NutritionLabelService)
     end
@@ -23,10 +23,11 @@ RSpec.describe 'Nutrition Label Service' do
 
   it 'returns an analyzed recipe' do
     VCR.use_cassette('analyzation') do
-      label = NutritionLabelService.analyze_recipe(recipe)
+      attrs = %i[yield calories totalWeight totalNutrients totalDaily]
+      label = NutritionLabelService.analyze_recipe(baguette)
 
-      require 'pry'; binding.pry
-      expect(label).to be_a(NutritionLabel)
+      expect(label).not_to be_empty
+      expect(attrs.all? {|s| label.key?(s)}).to be true
     end
   end
 end
