ad5f26472d651323daf5e0d9e98a66ffe3de92c2
Mike Heft <mikeheft@gmail.com>
2018-06-26 12:57:43 -0600

    Minor refactor for rubocop style

--- app/controllers/api/v1/nutrition_label_controller.rb
+++ app/controllers/api/v1/nutrition_label_controller.rb
@@ -4,6 +4,6 @@ class Api::V1::NutritionLabelController < Api::V1::ApplicationController
   def show
     recipe = Recipe.find_by(name: params[:recipe_name])
     recipe.fetch_label_info
-    render json: recipe.label
+    render(json: recipe.label)
   end
 end
