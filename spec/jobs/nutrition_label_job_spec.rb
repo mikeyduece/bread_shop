04f77cede61d380e792a545fb5edf90a069f43fd
Mike Heft <mikeheft@gmail.com>
2018-06-27 13:43:36 -0600

    Revert "Refactor queues the job test"
    This reverts commit 50ca07430a19fcc06d699c0b14b3098b9a9ce31e.

--- spec/jobs/nutrition_label_job_spec.rb
+++ spec/jobs/nutrition_label_job_spec.rb
@@ -3,18 +3,10 @@ require 'rails_helper'
 RSpec.describe NutritionLabelJob, type: :job do
   include ActiveJob::TestHelper
 
-  subject(:job) { described_class.perform_later }
+  subject(:job) { described_class.perform_later(123) }
 
   it 'queues the job' do
-    expect { job }.to have_enqueued_job(described_class).on_queue('default')
-  end
-
-  it 'is in default queue' do
-    expect(NutritionLabelJob.new.queue_name).to eq('default')
-  end
-
-  after do
-    clear_enqueued_jobs
-    clear_performed_jobs
+    expect { job }
+      .to change(ActiveJob::Base.queue_adapter.enqueued_jobs, :size).by(1)
   end
 end
