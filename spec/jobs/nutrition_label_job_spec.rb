b6a247e61c464ee1dcd937b43b1fd5c244bf71c0
Mike Heft <mikeheft@gmail.com>
2018-06-27 13:43:21 -0600

    Revert "Add test to show it queues the job for later"
    This reverts commit 7ca84b3e93c40bf17370499c7220ac031e54b194.

--- spec/jobs/nutrition_label_job_spec.rb
+++ spec/jobs/nutrition_label_job_spec.rb
@@ -5,13 +5,6 @@ RSpec.describe NutritionLabelJob, type: :job do
 
   subject(:job) { described_class.perform_later }
 
-  it 'queues for midnight' do
-    ActiveJob::Base.queue_adapter = :test
-    expect {
-      NutritionLabelJob.set(wait_until: Date.today.midnight, queue: 'default').perform_later
-    }.to have_enqueued_job.on_queue('default').at(Date.today.midnight)
-  end
-
   it 'queues the job' do
     expect { job }.to have_enqueued_job(described_class).on_queue('default')
   end
