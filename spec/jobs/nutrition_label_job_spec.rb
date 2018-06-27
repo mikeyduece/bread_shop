50ca07430a19fcc06d699c0b14b3098b9a9ce31e
Mike Heft <mikeheft@gmail.com>
2018-06-27 13:28:34 -0600

    Refactor queues the job test

--- spec/jobs/nutrition_label_job_spec.rb
+++ spec/jobs/nutrition_label_job_spec.rb
@@ -3,10 +3,18 @@ require 'rails_helper'
 RSpec.describe NutritionLabelJob, type: :job do
   include ActiveJob::TestHelper
 
-  subject(:job) { described_class.perform_later(123) }
+  subject(:job) { described_class.perform_later }
 
   it 'queues the job' do
-    expect { job }
-      .to change(ActiveJob::Base.queue_adapter.enqueued_jobs, :size).by(1)
+    expect { job }.to have_enqueued_job(described_class).on_queue('default')
+  end
+
+  it 'is in default queue' do
+    expect(NutritionLabelJob.new.queue_name).to eq('default')
+  end
+
+  after do
+    clear_enqueued_jobs
+    clear_performed_jobs
   end
 end
