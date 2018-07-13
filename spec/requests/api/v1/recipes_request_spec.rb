d18eb5d8f09363568195b03313f441f658a1a052
Mike Heft <mikeheft@gmail.com>
2018-07-13 16:21:10 -0600

    Add functionality to change how many records per page show up

--- spec/requests/api/v1/recipes_request_spec.rb
+++ spec/requests/api/v1/recipes_request_spec.rb
@@ -3,11 +3,12 @@
 require 'rails_helper'
 RSpec.describe 'Recipe Requests' do
   let!(:user) { create(:user_with_recipes) }
-  let!(:token) { TokiToki.encode(user.attributes) }
   let!(:recipe) { user.recipes[0] }
 
   it 'returns list of all recipes' do
     get '/api/v1/recipes'
+
+    expect(response).to be_successful
 
     recipes = JSON.parse(response.body, symbolize_names: true)
 
@@ -18,10 +19,62 @@ RSpec.describe 'Recipe Requests' do
   it 'returns specific recipe' do
     get "/api/v1/recipes/#{recipe.id}"
 
+    expect(response).to be_successful
+
     return_recipe = JSON.parse(response.body, symbolize_names: true)
 
 
     expect(return_recipe[:user][:name]).to eq(user.name)
     expect(return_recipe[:name]).to eq(recipe.name)
+  end
+
+  context 'Pagination' do
+    it 'can paginate' do
+      create_list(:recipe, 25)
+
+      get '/api/v1/recipes'
+
+      expect(response).to be_successful
+
+      recipes = JSON.parse(response.body, symbolize_names: true)
+
+      expect(recipes.count).to eq(10)
+
+      get '/api/v1/recipes', params: { page: 3 }
+
+      expect(response).to be_successful
+
+      paginated_recipes = JSON.parse(response.body, symbolize_names: true)
+
+      expect(paginated_recipes.count).to eq(9)
+    end
+
+    it 'can change per_page' do
+      create_list(:recipe, 50)
+
+      get '/api/v1/recipes', params: { per_page: 25}
+
+      expect(response).to be_successful
+
+      recipes = JSON.parse(response.body, symbolize_names: true)
+
+      expect(recipes.count).to eq(25)
+
+      get '/api/v1/recipes', params: { per_page: 5 }
+
+      expect(response).to be_successful
+
+      paginated_recipes = JSON.parse(response.body, symbolize_names: true)
+
+      expect(paginated_recipes.count).to eq(5)
+
+      get '/api/v1/recipes', params: { per_page: 30 }
+
+      expect(response).to be_successful
+
+      larger_return = JSON.parse(response.body, symbolize_names: true)
+
+      expect(larger_return.count).to eq(30)
+    end
   end
 end
