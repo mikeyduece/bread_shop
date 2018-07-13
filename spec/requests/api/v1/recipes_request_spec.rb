6d3e51450ecd1598241736dc242f6c23d2286a0e
Mike Heft <mikeheft@gmail.com>
2018-07-13 14:49:20 -0600

    Refactor spec to be more symantic and change routes to reflect new controllers and actions

--- spec/requests/api/v1/recipes_request_spec.rb
+++ spec/requests/api/v1/recipes_request_spec.rb
@@ -1,30 +1,27 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-RSpec.describe 'New recipe amount calculation' do
+RSpec.describe 'Recipe Requests' do
   let!(:user) { create(:user_with_recipes) }
   let!(:token) { TokiToki.encode(user.attributes) }
   let!(:recipe) { user.recipes[0] }
 
-  context 'Nutrition Label' do
-    it 'fetches label' do
-      VCR.use_cassette('label') do
-        recipe.recipe_ingredients.clear
-        %w[flour water salt yeast].each do |name|
-        ing = create(:ingredient, name: name)
-        recipe.recipe_ingredients << create(:recipe_ingredient, recipe: recipe, ingredient: ing)
-      end
-        attrs = %i[yield calories totalNutrients healthLabels totalDaily]
-        get "/api/v1/recipes/#{user.recipes[0].name}/label", params: {
-          token: token
-        }
+  it 'returns list of all recipes' do
+    get '/api/v1/recipes'
 
-        expect(response).to be_successful
+    recipes = JSON.parse(response.body, symbolize_names: true)
 
-        label = JSON.parse(response.body, symbolize_names: true)
+    expect(recipes.length).to eq(4)
+    recipes.each { |recipe| expect(recipe[:user][:name]).to eq(user.name) }
+  end
 
-        expect(attrs.all? { |s| label.key?(s) }).to be true
-      end
-    end
+  it 'returns specific recipe' do
+    get "/api/v1/recipes/#{recipe.id}"
+
+    return_recipe = JSON.parse(response.body, symbolize_names: true)
+
+
+    expect(return_recipe[:user][:name]).to eq(user.name)
+    expect(return_recipe[:name]).to eq(recipe.name)
   end
 end
