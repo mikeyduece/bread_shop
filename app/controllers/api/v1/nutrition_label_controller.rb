968bcb8dc01add105cf3f0ef898656356b375c0e
Mike Heft <mikeheft@gmail.com>
2018-07-29 14:20:09 -0600

    Refactor to use strong params

--- app/controllers/api/v1/nutrition_label_controller.rb
+++ app/controllers/api/v1/nutrition_label_controller.rb
@@ -2,8 +2,14 @@
 
 class Api::V1::NutritionLabelController < Api::V1::ApplicationController
   def show
-    recipe = Recipe.find(params[:id])
+    recipe = Recipe.find(recipe_id_params)
     recipe.fetch_label_info
     render(json: recipe.label)
+  end
+
+  private
+
+  def recipe_id_params
+    params.permit(:id)[:id]
   end
 end
