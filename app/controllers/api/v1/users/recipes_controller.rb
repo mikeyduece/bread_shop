7e178ec77ef267723ffd4af16660caffc12bf9ac
Mike Heft <mikeheft@gmail.com>
2018-07-31 07:17:20 -0600

    Remove commented out code

--- app/controllers/api/v1/users/recipes_controller.rb
+++ app/controllers/api/v1/users/recipes_controller.rb
@@ -62,28 +62,4 @@ class Api::V1::Users::RecipesController < Api::V1::ApplicationController
   def recipe_name_param
     params.require(:recipe).permit(:name)[:name]
   end
-
-  #def tag_list
-  #  @recipe.tags << Tag.create_list(params[:tags])
-  #end
-
-  #def assign_family
-  #  @recipe.assign_family
-  #end
-
-  #def recipe_ingredient_list
-  #  @recipe.recipe_ingredient_list(recipe_ing_params)
-  #end
-
-  #def ingredient_list
-  #  Ingredient.create_list(ingredient_params)
-  #end
-
-  #def recipe_activity
-  #  user = current_user.id
-  #  client = Stream::Client.new(ENV['STREAM_KEY'], ENV['STREAM_SECRET'])
-  #  feed = client.feed('user', user)
-  #  activity_data = { actor: user, verb: 'post', object: 1, post: "#{current_user.name} created a new recipe" }
-  #  feed.add_activity(activity_data)
-  #end
 end
